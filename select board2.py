0f387432576cd2d8cfdc3d48ffaa4c1fc1eaf656
bang <057310igo@gmail.com>
2023-07-07 15:21:39 +0800

    re

--- select board2.py
+++ select board2.py
@@ -3,6 +3,8 @@ import datetime
 import requests
 import re
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 def scrape_articles(board):
@@ -19,7 +21,7 @@ def scrape_articles(board):
 
     with open(filename, "w", encoding="utf-8", newline="") as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(["標題", "發文時間", "作者", "內文"])
+        writer.writerow(["標題", "發文時間", "作者", "內文", "留言"])
 
         for article_element in article_elements:
             title_element = article_element.select_one(".title")
@@ -51,10 +53,32 @@ def scrape_articles(board):
             # 移除标题、看板名称以及相关HTML标签
             content = re.sub(re.escape(title), "", content)
             content = re.sub(r"\s*(<[^>]+>|※\s*(發信站|看板|文章網址|編輯):\s*.*|--.*)\s*", "", content)
-            content = content.replace("看板" + board, "")
+            content = content.replace("看板 " + board, "")
+            # 精簡內容
+            content = re.sub(r"作者.*", "", content)
+            content = re.sub(r"看板.*", "", content)
+            content = re.sub(r"標題.*", "", content)
+            content = re.sub(r"時間.*", "", content)
+            content = re.sub(r"https:.*", "", content)
+            content = re.sub(r"※ 發信站:.*", "", content)
+            content = re.sub(r"※ 文章網址:.*", "", content)
+            content = re.sub(r"※ 編輯:.*", "", content)
+            content = re.sub(r"--.*", "", content)
+            content = content.replace(author, "").replace(post_time_str, "").replace(title, "")
+            lines = content.strip().splitlines()
+            cleaned_content = " ".join(line for line in lines if line.strip())
+            cleaned_content = re.sub(r'//.*', '', cleaned_content)
+            # 提取留言部分
+            #將原始碼做整理
+            soup = BeautifulSoup(response.text, 'lxml')
+            comments = soup.find_all('div', 'push')
+            #取得留言內容
+            for article in comments:
+                messages = article.find('span', 'f3 push-content').getText()
+           
 
             # 写入CSV文件
-            writer.writerow([title, post_time_str, author, content])
+            writer.writerow([title, post_time_str, author, content, comments])
 
     print(f"爬取完成，结果已保存在{filename}中。")
 
