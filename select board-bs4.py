1f3b6aa36b705c1515750623952eb3a4429df476
bang <057310igo@gmail.com>
2023-07-11 10:25:32 +0800

    modify

--- select board-bs4.py
+++ select board-bs4.py
@@ -1,16 +1,18 @@
-# 引入所需函示庫
 import csv
 import datetime
 import requests
 import bs4
 import re
-# 定義scrape_article函數
-def scrape_article(url):# 接收url
+
+def scrape_article(url):
     my_headers = {'cookie': 'over18=1;'}
-    response = requests.get(url, headers=my_headers)# 發送requests獲取頁面內容
-    soup = bs4.BeautifulSoup(response.text, "html.parser")# 解析頁面
+    response = requests.get(url, headers=my_headers)
+    soup = bs4.BeautifulSoup(response.text, "html.parser")
 
     header = soup.find_all('span', 'article-meta-value')
+    if len(header) < 4:
+        return None, None, None, None, None, None
+
     author = header[0].text
     board = header[1].text
     title = header[2].text
@@ -27,26 +29,30 @@ def scrape_article(url):# 接收url
     push_elements = soup.select(".push")
     for push_element in push_elements:
         comment = push_element.select_one(".push-content").text.strip()
-        comment=comment.replace(": ","")
+        comment = comment.replace(": ", "")
         comments.append(comment)
+
     return board, title, author, date, content, comments
-# 定義scrape_articles函數
-def scrape_articles(board, target_date=None):# board表示要爬取的版面，target_date表示指定日期(默認為當天)
+
+def scrape_articles(board, target_year=None, target_date=None):
+    if target_year is None:
+        target_year = datetime.date.today().year
     if target_date is None:
         target_date = datetime.date.today()
     else:
         target_date = datetime.datetime.strptime(target_date, "%Y-%m-%d").date()
+        target_date = target_date.replace(year=target_year)
 
     url = f"https://www.ptt.cc/bbs/{board}/index.html"
     my_headers = {'cookie': 'over18=1;'}
     count = 0
     page = 0
-    filename = f"{board}_articles_{target_date}.csv"# 打開CSV文件，並使用"csv.writer"創建一個寫入器
-    
+    filename = f"{board}_articles_{target_year}_{target_date}.csv"
+
     with open(filename, "w", encoding="utf-8", newline="") as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(["看板", "標題", "作者", "發文時間", "內容", "留言"])
-        # 使用循環撥放，直到滿足100篇或沒有更多頁面
+
         while count < 100:
             response = requests.get(url, headers=my_headers)
             soup = bs4.BeautifulSoup(response.text, "html.parser")
@@ -55,39 +61,44 @@ def scrape_articles(board, target_date=None):# board表示要爬取的版面，t
             for article in articles:
                 if count >= 100:
                     break
-                # 使用CSS選擇器逐一處理
+
                 title_element = article.select_one(".title a")
                 if title_element is None:
                     continue
 
                 title = title_element.text.strip()
-                # 檢查文章的日期是否滿足指定日期，若不滿足則跳過
+
                 date_element = article.select_one(".meta .date")
                 if date_element:
                     date_str = date_element.text.strip()
                     date = datetime.datetime.strptime(date_str, "%m/%d").date()
-                    date = date.replace(year=target_date.year)
+                    date = date.replace(year=target_year)
                 else:
                     date = datetime.date.today()
 
+                if date.year != target_year:
+                    continue
+
                 if date != target_date:
                     continue
-                # 獲取文章連結，並調用"scrape_article"函數處理頁面
+
                 article_url = "https://www.ptt.cc" + title_element["href"]
                 board, title, author, date, content, comments = scrape_article(article_url)
-                writer.writerow([board, title, author, date, content, comments])# 將提取的訊息寫入csv
+                if board is None:
+                    continue
 
-                count += 1 #更新記數器
+                writer.writerow([board, title, author, date, content, comments])
+                count += 1
 
-            page += 1 # 或取下一頁的連結
+            page += 1
             next_link = soup.select_one(".btn-group-paging a:nth-child(2)")
             if next_link:
                 url = "https://www.ptt.cc" + next_link["href"]
             else:
-                break # 當循環結束，關閉CSV
+                break
 
     print(f"爬取完成，结果已保存在{filename}中。")
 
 
-# 示例：爬取指定日期（2023-07-10）的最新100篇文章，如果没有指定日期則爬取當天的最新100篇内容
-scrape_articles("Gossiping", target_date="2023-07-10")
+# 示例：爬取指定年份（2023）和日期（07-10）的最新100篇文章，如果没有指定日期则爬取当年当天的最新100篇内容
+scrape_articles("Gossiping", target_year=2023, target_date="2023-07-10")
